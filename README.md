16216dfe34a5687b34cf191e75c1552b2d1def49
gio256 <102917377+gio256@users.noreply.github.com>
2024-07-03 20:27:58 -0600

    Add stacked borrows debugging tools

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 Convenient examples of [undefined behavior](https://raphlinus.github.io/programming/rust/2018/08/17/undefined-behavior.html) in rust.
 
 ### Usage
-Unless otherwise stated, every test should fail when run with [Miri](https://github.com/rust-lang/miri).
+Unless otherwise stated, every test should exhibit UB when run with [Miri](https://github.com/rust-lang/miri).
 
 ```
 cargo miri test ptr::test_no_provenance -- --exact
 ```
+
+For the most part, tests that do not exhibit UB are named `test_ok_*`.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[cfg(test)]
 mod ptr {
     use core::ptr;
 
@@ -21,18 +22,92 @@ mod ptr {
     #[test]
     fn test_double_drop() {
         let x = Box::new(1);
-        let _y: Box<usize> = unsafe { ptr::read(&x) };
-        panic!("bad place to panic") // UB
+        let _y = unsafe { ptr::read(&x) };
+        panic!("bad place to panic"); // UB
     }
 }
 
+#[cfg(test)]
+mod validity {
+    use core::mem::transmute;
+
+    #[test]
+    fn test_bad_bool() {
+        let x = 2_u8;
+        #[allow(clippy::transmute_int_to_bool)]
+        let _: bool = unsafe { transmute(x) };
+    }
+}
+
+#[cfg(test)]
 mod borrows {
     use core::cell::UnsafeCell;
 
+    extern "Rust" {
+        fn miri_get_alloc_id(ptr: *const u8) -> u64;
+        fn miri_print_borrow_state(alloc_id: u64, show_unnamed: bool);
+    }
+
+    fn get_alloc_id(ptr: *const u8) -> u64 {
+        unsafe { miri_get_alloc_id(ptr) }
+    }
+
+    fn print_borrow_stacks(alloc_id: u64) {
+        unsafe {
+            miri_print_borrow_state(alloc_id, true)
+        }
+    }
+
+    #[test]
+    fn test_reborrow_dbg() {
+        let mut val = 1_u8;
+        let alloc_id = get_alloc_id(&val as *const u8);
+        print_borrow_stacks(alloc_id);
+
+        let x: *mut u8 = &mut val;
+        print_borrow_stacks(alloc_id);
+
+        // let _y: *mut u8 = unsafe { &mut *x }; // ok
+        let _y: *mut u8 = &mut val; // not ok
+        print_borrow_stacks(alloc_id);
+
+        let _ = unsafe { *x };
+        print_borrow_stacks(alloc_id);
+    }
+
+    /// This is UB. The parent of `_y` is `val`, which pops x off the stack.
+    #[test]
+    fn test_reborrow() {
+        let mut val = 1_u8;
+        let x: *mut u8 = &mut val;
+        let _y: *mut u8 = &mut val; // not ok
+        let _ = unsafe { *x }; // UB
+    }
+
+    /// This is not UB. The parent of `_y` is now `x`, so `x` stays on the stack.
+    #[test]
+    fn test_ok_reborrow() {
+        let mut val = 1_u8;
+        let x: *mut u8 = &mut val;
+        let _y: *mut u8 = unsafe { &mut *x }; // ok
+        let _ = unsafe { *x };
+    }
+
     #[test]
     fn test_cell() {
-        let x = &0_usize as *const usize;
-        let cell = x as *const UnsafeCell<usize>;
+        let x = 0_usize;
+        let cell = &x as *const usize as *const UnsafeCell<usize>;
         let _ = unsafe { &*cell }; // UB
     }
+
+    #[test]
+    fn test_ok_interleave_reads() {
+        let mut val = 1_u8;
+        let u: *mut u8 = &mut val;
+        let s: *const u8 = unsafe { &*u };
+        let _ = unsafe { *s };
+        let _ = unsafe { *u };
+        let _ = unsafe { *s };
+    }
+
 }
