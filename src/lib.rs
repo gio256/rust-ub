cccb50f02f50d8f9c7339952e6ddce83eb1e3baa
gio256 <102917377+gio256@users.noreply.github.com>
2024-07-22 10:19:20 -0600

    Reorder modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,4 @@
 #[cfg(test)]
-mod ptr {
-    use core::ptr;
-
-    #[test]
-    fn test_no_provenance() {
-        let p = ptr::NonNull::<u8>::dangling();
-        // memory access failed: 0x1[noalloc] is a dangling pointer (it has no provenance)
-        let _ = unsafe { *p.as_ptr() }; // UB
-    }
-
-    #[test]
-    fn test_oob() {
-        let arr: &[u8; 4] = &[0, 1, 2, 3];
-        let p = arr as *const u8;
-        // 1 byte past the end is okay
-        let q = unsafe { p.add(arr.len()) };
-        // any further is UB: out-of-bounds pointer arithmetic
-        let _ = unsafe { q.offset(1) }; // UB
-    }
-
-    /// Run with `-Zmiri-symbolic-alignment-check` to reliably detect UB.
-    #[test]
-    fn test_unaligned() {
-        // u32 has alignment 4 (on most platforms).
-        #[repr(C, packed)]
-        struct Packed(pub u8, pub u32);
-        let packed = Packed(1, 2);
-        let unaligned = ptr::addr_of!(packed.1);
-        let _ = unsafe { *unaligned }; // UB
-    }
-
-    #[test]
-    fn test_double_drop() {
-        let x = Box::new(1);
-        let _y = unsafe { ptr::read(&x) };
-        panic!("bad place to panic"); // UB
-    }
-}
-
-#[cfg(test)]
 mod borrows {
     use core::cell::UnsafeCell;
     use core::ptr;
@@ -68,12 +28,6 @@ mod borrows {
     fn test_dbg() {
         let mut val = 1_u8;
         let alloc = get_alloc_id(&val as *const u8);
-        dbg_borrows(alloc);
-
-        let x = &mut val as *const u8 as *mut u8;
-        dbg_borrows(alloc);
-
-        unsafe { *x = 2 };
         dbg_borrows(alloc);
     }
 
@@ -228,6 +182,47 @@ mod borrows {
         let _ = unsafe { *s };
     }
 }
+
+#[cfg(test)]
+mod ptr {
+    use core::ptr;
+
+    #[test]
+    fn test_no_provenance() {
+        let p = ptr::NonNull::<u8>::dangling();
+        // memory access failed: 0x1[noalloc] is a dangling pointer (it has no provenance)
+        let _ = unsafe { *p.as_ptr() }; // UB
+    }
+
+    #[test]
+    fn test_oob() {
+        let arr: &[u8; 4] = &[0, 1, 2, 3];
+        let p = arr as *const u8;
+        // 1 byte past the end is okay
+        let q = unsafe { p.add(arr.len()) };
+        // any further is UB: out-of-bounds pointer arithmetic
+        let _ = unsafe { q.offset(1) }; // UB
+    }
+
+    /// Run with `-Zmiri-symbolic-alignment-check` to reliably detect UB.
+    #[test]
+    fn test_unaligned() {
+        // u32 has alignment 4 (on most platforms).
+        #[repr(C, packed)]
+        struct Packed(pub u8, pub u32);
+        let packed = Packed(1, 2);
+        let unaligned = ptr::addr_of!(packed.1);
+        let _ = unsafe { *unaligned }; // UB
+    }
+
+    #[test]
+    fn test_double_drop() {
+        let x = Box::new(1);
+        let _y = unsafe { ptr::read(&x) };
+        panic!("bad place to panic"); // UB
+    }
+}
+
 
 #[cfg(test)]
 mod concurrency {
